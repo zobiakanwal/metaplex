7723a50082490ebb565e98513f31330887292928
zobiakanwal <xobiaaijaz@gmail.com>
2022-01-30 04:18:55 +0500

    Updates

---
+++ _next/static/PPAtYL4dWBRNFjxszc0RH/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[{source:"/:any*",destination:"/"}],fallback:[]},"/":["static/chunks/pages/index-c8ef3c4c98c29efae522.js"],"/_error":["static/chunks/pages/_error-077790e38fbfe3891eb2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
